15bdeafa9600186bee5d28079e5c845df0b82051
jess <jlfinlayson4@gmail.com>
2019-07-02 21:32:05 -0400

    "moved code to be logical. psuedo coded section i am unsure of."

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var redCrystal = 0;
 var greenCrystal = 0;
 var orangeCrystal = 0;
 
-// Function to generate crystal and total values
+// Function to generate crystal and total values at document ready
 
 $(document).ready(function () {
     console.log("start");
@@ -21,27 +21,15 @@ $(document).ready(function () {
     console.log("target score = " + target);
 });
 
-function scoreRules() {
-    if (sumGuess === target) {
-        wins++;
-        $("wins").text(wins);
-        console.log("win");
-    } else if (sumGuess > target) {
-        losses++;
-        $("losses").text(losses);
-        console.log("loss");
-    }
-};
+// Game reset function after wins/losses
 
 function reset() {
-    var target = 0;
-    var sumGuess = 0;
-    var wins = 0;
-    var losses = 0;
-    var blueCrystal = 0;
-    var redCrystal = 0;
-    var greenCrystal = 0;
-    var orangeCrystal = 0;
+    target = 0;
+    sumGuess = 0;
+    blueCrystal = 0;
+    redCrystal = 0;
+    greenCrystal = 0;
+    orangeCrystal = 0;
 
     console.log("start");
     blueCrystal = Math.floor(Math.random() * 10) + 1;
@@ -53,3 +41,21 @@ function reset() {
     $("#target-number").text(target);
     console.log("target score = " + target);
 }
+
+// Rules to tell when you win or lose and what to do
+
+function scoreRules() {
+    if (sumGuess === target) {
+        wins++;
+        $("wins").text(wins);
+        console.log("win");
+        reset();
+    } else if (sumGuess > target) {
+        losses++;
+        $("losses").text(losses);
+        console.log("loss");
+        reset();
+    }
+};
+
+// Crystal images have values and can be clicked
